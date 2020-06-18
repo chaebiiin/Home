7b4304f2a53285a74955697d05ac4a70d8cdb360
cheabiiin <ravis_toy@naver.com>
2020-06-18 20:23:05 +0900

    20-06-18

---
+++ Home/src/sec05/exam04_function/FunctionExample.java
@@ -0,0 +1,37 @@
+package sec05.exam04_function;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.ToIntFunction;
+
+public class FunctionExample {
+	private static List<Student>list = Arrays.asList(
+			new Student("김원식", 99, 88),
+			new Student("한상혁", 65, 95)
+			);
+	
+	public static void printString(Function<Student, String> function) {
+		for(Student student : list) {
+			System.out.print(function.apply(student) + " ");
+		}
+		System.out.println();
+	}
+	public static void printInt(ToIntFunction<Student> function) {
+		for(Student student : list) {
+			System.out.print(function.applyAsInt(student) + " ");
+		}
+		System.out.println();
+	}
+	public static void main(String[] args) {
+		System.out.println("학생 이름 :  ");
+		printString(t -> t.getName());
+		
+		System.out.println("영어점수 : ");
+		printInt(t -> t.getEnglishScore());
+		
+		System.out.println("수학 점수 : ");
+		printInt(t -> t.getMathScore());
+	}
+
+}
